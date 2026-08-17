2c23af381e246d4a38a3ca53baaba31c7d26a266
Dharmendra Upadhyay <cellplan.dh@gmail.com>
2026-08-18 00:20:58 +0530

    Add Terraform CI workflow

--- module/azurerm_resource_group/main.tf
+++ module/azurerm_resource_group/main.tf
@@ -2,7 +2,7 @@ variable "rfg" {}
 
 resource "azurerm_resource_group" "rgh" {
   for_each = var.rfg
-  location = each.value.name
+  location = each.value.location
   name     = each.value.name
 }
 
